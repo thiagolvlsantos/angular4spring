235c68656382e1af903ca2a471d9459da64f4d37
Thiago Santos <thiago.lvl.santos@gmail.com>
2017-09-22 09:33:29 -0300

    Project start.

---
+++ angular4spring-server/src/main/java/com/thiagolvlsantos/Angular4springApplication.java
@@ -0,0 +1,12 @@
+package com.thiagolvlsantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular4springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular4springApplication.class, args);
+	}
+}
