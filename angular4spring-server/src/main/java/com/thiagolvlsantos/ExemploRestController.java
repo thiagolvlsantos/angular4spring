d4c0f305c71fece2c1a33491353845ae0154c6f9
Thiago Santos <thiago.lvl.santos@gmail.com>
2017-09-24 01:00:55 -0300

    test

--- angular4spring-server/src/main/java/com/thiagolvlsantos/ExemploRestController.java
+++ angular4spring-server/src/main/java/com/thiagolvlsantos/ExemploRestController.java
@@ -1,14 +1,14 @@
 package com.thiagolvlsantos;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/angular4spring")
 public class ExemploRestController {
 
 	@RequestMapping(path = "/echo")
-	public String echo(@PathVariable(name = "msg") String msg) {
+	public String echo(@RequestParam(name = "msg") String msg) {
 		return "Echo: ..." + msg;
 	}
 }
